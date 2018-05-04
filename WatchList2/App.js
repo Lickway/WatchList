68959af19b211c0ea2733d4e415b6946b7dc055c
Jordan Liekweg <jordanliekweg@ool-ad0203c5.dyn.optonline.net>
2018-05-04 16:16:15 -0400

    Added firebase

--- WatchList2/App.js
+++ WatchList2/App.js
@@ -8,6 +8,17 @@ import {
 import { StackNavigator } from "react-navigation";
 import HomeScreen from "./Components/Home/HomeScreen";
 import SearchTabNavigator from "./Components/Search/SearchTabNavigator";
+import * as firebase from "firebase";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyB1WHUpgphGI14IXJrV_jMPwi71HZ5LOw0",
+  authDomain: "watchlist-c196d.firebaseapp.com",
+  databaseURL: "https://watchlist-c196d.firebaseio.com",
+  projectId: "watchlist-c196d",
+  storageBucket: "watchlist-c196d.appspot.com",
+  messagingSenderId: "463334371065"
+}
+firebase.initializeApp(firebaseConfig);
 
 console.ignoredYellowBox = ['Remote debugger'];
 
@@ -17,7 +28,7 @@ const App = StackNavigator({
   SearchTabNavigator: { screen : SearchTabNavigator }
 
 },{
-  initialRouteName: 'SearchTabNavigator'
+  initialRouteName: 'HomeScreen'
 })
 
 export default App;
